cae6b4f46be32eff02cbe6594ed3e7254f7ec0cb
rajatenzyme <rajatmittal2012@gmail.com>
2021-09-01 01:29:35 +0530

    Constructors,static

---
+++ OOPS-codes/15.Constructors_Overloading.cpp
@@ -0,0 +1,33 @@
+
+/*-------------------------------------------Constructor Overloading---------------------------------*/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+class Complex{
+    private:
+    int a, b;
+
+    public:
+    Complex(){
+        cout<<"Constructor without parameters"<<endl;
+    }
+    Complex(int x, int y){
+        a = x;
+        b = y;
+    }
+
+    Complex (int k){   // Constructor overloading (same method name, different arguments)
+        a = k;
+    }
+};
+
+int main(){
+    Complex c1(3,5);
+    Complex c2(5);
+    //for single value we can also write it as:
+    Complex c5 = 6; //Valid only for single value constructor
+    Complex c3; 
+    Complex c4 = Complex(1,5); // different way to write - same meaning
+
+}
